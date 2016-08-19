c130b996eb5dc5f389ace64d76cb172401a28bb1
J. Haskins <ziopads@gmail.com>
2016-08-19 07:02:39 -0600

    start schema

---
+++ migrations/20160819062505_createSchema.js
@@ -0,0 +1,46 @@
+
+exports.up = function(knex, Promise) {
+  return PromiseAll([
+    knex.schema.createTable('users', (table) => {
+      table.increments();
+    }),
+    knex.schema.createTable('roles', (table) => {
+      table.increments();
+    }),
+    knex.schema.createTable('permissions', (table) => {
+      table.increments();
+    }),
+    knex.schema.createTable('users_roles', (table) => {
+      table.increments();
+    }),
+    knex.schema.createTable('roles_permissions', (table) => {
+      table.increments();
+    }),
+    knex.schema.createTable('games', (table) => {
+      table.increments();
+    }),
+    knex.schema.createTable('gameEvents', (table) => {
+      table.increments();
+    }),
+    knex.schema.createTable('eventTypes', (table) => {
+      table.increments();
+    }),
+    knex.schema.createTable('games_gameEvents', (table) => {
+      table.increments();
+    }),
+    knex.schema.createTable('gameEvents_eventTypes', (table) => {
+      table.increments();
+    }),
+    knex.schema.createTable('activePlayers', (table) => {
+      table.increments();
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return PromiseAll([
+    knex.raw('drop table if exists users cascade')
+    knex.raw('drop table if exists games cascade')
+    knex.raw('drop table if exists activePlayers cascade')
+  ])
+};
